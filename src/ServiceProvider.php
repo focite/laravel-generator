fbc8f93ac1e9e6f8cb1d4552e5f26e17b8f452e9
wanganlin <inbox@wanganlin.com>
2023-05-22 17:20:02 +0800

    增加 service provider

---
+++ src/ServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Focite\Builder;
+
+use Focite\Builder\Console\Commands\GenEntity;
+use Focite\Builder\Console\Commands\GenModel;
+use Focite\Builder\Console\Commands\GenRepository;
+use Focite\Builder\Console\Commands\GenService;
+
+class ServiceProvider
+{
+    /**
+     * Bootstrap any package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+       
+    }
+
+    /**
+     * Bootstrap any package services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+			$this->commands([
+				GenEntity::class,
+				GenModel::class,
+				GenRepository::class,
+				GenService::class,
+			]);
+		}
+    }
+}
