3f945ddec50e10eae09c531ccdf61488d5b638fa
wanganlin <inbox@wanganlin.com>
2023-08-15 10:00:45 +0800

    优化代码

--- stubs/app/Http/Controllers/Controller.php
+++ stubs/app/Http/Controllers/Controller.php
@@ -10,7 +10,7 @@ use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Routing\Controller as BaseController;
 
-class Controller extends BaseController
+abstract class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
 
